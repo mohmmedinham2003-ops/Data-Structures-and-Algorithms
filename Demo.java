0f120d1a7975008fdd8ae6c7c70b53ca37907c92
Mohammed Inham <mohmmedinham2003@gmail.com>
2026-07-09 22:33:17 +0530

    Implement Stack class and demo push functionality
    Added a Stack class and implemented push and display methods in the Demo class.

--- Demo.java
+++ Demo.java
@@ -1,1 +1,34 @@
+class Stack{
+	
+	
+	
+}
+
+
+class Demo{
+	public static void main (String args[]){
+			Stack s1 = new Stack(12,0.25);
+			s1.push(10);
+			s1.push(20);
+			s1.push(30);
+			s1.push(40);
+			s1.push(50);
+			s1.push(60);
+			s1.push(70);
+			s1.push(80);
+			s1.push(90);
+			s1.push(100);
+			s1.push(110);
+			s1.push(120);
+			
+			s1.display();
+			System.out.println("Size :"+s1.size());
+		
+		
+		
+		}
+	
+	
+	
+	}
 
