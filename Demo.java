426887f3f46fc38ceb43b92298c93c301bdfcd9f
Mohammed Inham <mohmmedinham2003@gmail.com>
2026-07-10 15:22:55 +0530

    Refactor Stack class and add Queue class

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
-class Stack{
-	
+//Stack over All execution
+/*class Stack{
 	private int[] dataArray;
 	private int nextIndex;
 	private double loadFact;
@@ -10,84 +10,160 @@ class Stack{
 		nextIndex=0;
 		this.loadFact=loadFact;
 		this.initSize=initSize;
-		
 	}
-	
-	private void extendArray(){
-	int tempDataArray[] = new int[dataArray.length+(int)(loadFact*dataArray.length)];
-	for(int i = 0;i<dataArray.length;i++){
-		
-		tempDataArray[i] = dataArray[i];
+	private void extendsArray(){
+		int tempDataArray[]=new int[dataArray.length+(int)(loadFact*dataArray.length)];
+		for (int i = 0; i < dataArray.length; i++){
+			tempDataArray[i]=dataArray[i];
 		}
-		dataArray = tempDataArray;
-		
+		dataArray=tempDataArray; 
 	}
-	
 	private boolean isFull(){
-	return nextIndex >= dataArray.length;	
+		return nextIndex>=dataArray.length;
 	}
-	
-	//insert all peek,push,pop
+	public int peek(){
+		return isEmpty() ? -1: dataArray[nextIndex-1]; 
+	}
+	public int poll(){
+		return isEmpty() ? -1:  dataArray[--nextIndex]; 
+	}
 	public void push(int data){
-	if(isFull()){
-	extendArray();	
-	}	
-	dataArray[nextIndex++] = data;	
+		if(isFull()){
+			extendsArray();
+		}
+		dataArray[nextIndex++]=data;
 	}
-	
+	public void trimToSize(){
+		int[] tempDataArray=new int[size()];
+		for (int i = 0; i < dataArray.length; i++){
+			tempDataArray[i]=dataArray[i];
+		}
+		dataArray=tempDataArray;
+	}
+	public int size(){
+		return nextIndex;
+	}
+	public void trim(){
+		//
+		//
+	}
+	public void clear(){
+		dataArray=new int[initSize];
+		nextIndex=0;
+	}
 	public void pop(){
 		if(!isEmpty()){
-		nextIndex--;	
+			nextIndex--;
 		}
+	}
+	public int search(int data){
+		//Insert code here
+		return -1;
+	}
+	public void display(){
+		System.out.print("[");
+		for (int i =nextIndex-1; i>=0 ; i--){
+			System.out.print(dataArray[i]+", ");
+		}
+		System.out.println(isEmpty()? "empty]":"\b\b]");
+	}
+	public boolean isEmpty(){
+		return nextIndex<=0;
+	}
+	public int capacity(){
+		return dataArray.length;
+	}
+}
+class Demo{
+	public static void main(String args[]){
+		Stack s1=new Stack(12,0.25);
+		s1.push(10);
+		s1.push(20);
+		s1.push(30);
+		s1.push(40);
+		s1.push(50);
+		s1.push(60);
+		s1.push(70);
+		s1.push(80);
+		s1.push(90);
+		s1.push(100);
+		s1.push(110);
+		s1.push(120);
+		s1.display(); //[120,110,100,90...50,40,30,20,10]
+		System.out.println("Size      : "+s1.size()); //12
+		
+		int topData;
+		topData=s1.peek();
+		System.out.println("Top Data : "+topData); //120
+		System.out.println("After calling peek()...");
+		s1.display(); //[120,110,100,90...50,40,30,20,10]
+		
+		topData=s1.poll();
+		System.out.println("Top Data : "+topData); //120
+		System.out.println("After calling poll()...");
+		s1.display(); //[110,100,90...50,40,30,20,10]
 		
 	}
+}
+*/
+
+//Queue Execution
+class Queue{
+	private int[] dataArray;
+	private int nextIndex;
+	private double loadFact;
+	private int initSize;
 	
-	public int peek(){
-		return isEmpty() ?-1:dataArray[nextIndex-1];
+	Queue(int initSize,double loadFact){
+		dataArray = new int[initSize];
+		nextIndex = 0;
+		this.loadFact = loadFact;
+		this.initSize = initSize;
 	}
 	
-	public int poll(){
-	return isEmpty() ?-1:dataArray[--nextIndex];	
-	}
-	
-	public int size(){
-	return nextIndex;	
-	}
-	
-	public void trimToSize(){
-	int[]	tempDataArray = new int[size()];
+	private void extendsArray(){
+	int tempDataArray[] = new int[dataArray.length+(int)(loadFact*dataArray.length)];
 	for(int i = 0;i<dataArray.length;i++){
 	tempDataArray[i] = dataArray[i];	
 	}
 	dataArray = tempDataArray;
-		
 	}
 	
-	public void clear(){
-	dataArray = new int[initSize];
-	nextIndex = 0;	
-	}
 	
-	public int seach(int data){
-	return -1;		
-	}
 	
-	public void display(){
-	System.out.print("[");
-	for(int i = nextIndex-1;i>=0;i--){
-	System.out.print(dataArray[i]+", ");	
-	}
-	System.out.print(isEmpty()?"empty]":"\b\b]");	
-		
-	}
 	
-	public boolean isEmpty(){
-	return nextIndex <=0;	
-	}
 	
-	public int capacity(){
-	return dataArray.length;	
-	}
+	
+	
+	
+	
+}
+
+
+class Demo{
+		public static void main(String args[]){
+			Queue q1 = new Queue(12,0.5);
+			q1.add(10);
+			q1.add(20);
+			q1.add(30);
+			q1.add(40);
+			q1.add(50);
+			q1.add(60);
+			q1.add(70);
+			q1.add(80);
+			q1.add(90);
+			q1.add(100);
+			q1.add(110);
+			q1.add(120);
+			q1.display(); //[10,20,30,40,50 ........120]
+			System.out.println("Size  :"+q1.size());
+			
+			q1.remove();
+			q1.display();
+			
+			
+			
+		}
 	
 	
 	
@@ -96,37 +172,27 @@ class Stack{
 
 
 
-class Demo{
-	public static void main (String args[]){
-			Stack s1 = new Stack(12,0.25);
-			s1.push(10);
-			s1.push(20);
-			s1.push(30);
-			s1.push(40);
-			s1.push(50);
-			s1.push(60);
-			s1.push(70);
-			s1.push(80);
-			s1.push(90);
-			s1.push(100);
-			s1.push(110);
-			s1.push(120);
-			
-			s1.display();
-			System.out.println("Size :"+s1.size());
-		
-			int topData;
-			topData = s1.peek();
-			System.out.println("Top data" + topData);//120
-			s1.display(); //[120,110,100,90...50,40,30,20,10]
 
-			topData = s1.poll();
-			System.out.println("Top data: "+topData);//120
-			System.out.println("Top data" + topData);//120
-			s1.display(); //[120,110,100,90...50,40,30,20,10]
-		
-		}
-	
-	
-	
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
