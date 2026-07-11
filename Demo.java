1e3215899974fce6f2331187c78fabc7e6e036f9
Mohammed Inham <mohmmedinham2003@gmail.com>
2026-07-11 19:23:00 +0530

    Implement Student class with constructors and methods
    Added Student class with properties and methods.

--- Demo.java
+++ Demo.java
@@ -385,7 +385,33 @@ class Demo{
 //List Example
 
 class Student{
+	private String id;
+	private String name;
+	private int prfMarks;
+	private int dbmsMarks;
 	
+	Student(){
+		
+	}
+	
+	Student(String id,String name,int prfMarks,int dbmsMarks){
+		this.id = id;
+		this.name = name;
+		this.prfMarks = prfMarks;
+		this.dbmsMarks = dbmsMarks;
+	}
+	
+	public String getId(){
+	return id;	
+	}
+	
+	public boolean equals(Student student){
+	return id.equalsIgnoreCase(student.id);	
+	}
+	public String toString(){
+	return "["+id+","+name+","+prfMarks+","+dbmsMarks+"]";	
+	}
+
 }
 
 class StudentList{
