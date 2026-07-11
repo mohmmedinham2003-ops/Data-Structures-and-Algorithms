cc4050c927593c3b04b981821cbcb2b8928f8e24
Mohammed Inham <mohmmedinham2003@gmail.com>
2026-07-11 20:13:00 +0530

    Implement StudentList class with dynamic array management

--- Demo.java
+++ Demo.java
@@ -415,10 +415,95 @@ class Student{
 }
 
 class StudentList{
+	private Student[] studentArray;
+	private int nextIndex;
+	private double loadFact;
+	private int initSize;
+	
+	private void extendsArray(){
+	Student tempStudentArray[]=new Student[studentArray.length+(int)(loadFact*studentArray)];
+	for(int i =0;i<studentArray.length;i++){
+			tempStudentArray[i]=studentArray[i];
+	}
+	studentArray = tempStudentArray;
+	}
+	
+	private boolean isFull(){
+	return nextIndex>=studentArray.length;	
+	}
+	
+	public void add(Student student){
+		if(isFull()){
+			extendsArray();
+		}
+		studentArray[index] = student;
+		nextIndex++;
+	}
+	
+	public void add(int index,Student student){
+		if(isFull()){
+		extendsArray();	
+		}
+		if(index >=0 && index <= nextIndex){
+		for(int i=nextIndex-1;i >=index;i--){
+		studentArray[i+1] = studentArray[i];	
+		}
+			studentArray[index] = student;
+			nextIndex++;
+			
+		}
+		
+	}
+	public void addFirst(Student student){
+		add(0,student);
+	}
+	public void addLast(Student student){
+		add(nextIndex,student);
+	}
+	public void trimSize(){
+		Student[] tempStudentArray = new Student[size()];
+		for (int i = 0; i < studentArray.length; i++){
+			tempStudentArray[i]=studentArray[i];
+		}
+		studentArray=tempStudentArray;
+	}
+	
+	public int size(){
+		return nextIndex;
+	}
+	
+	public void clear(){
+	studentArray = new Student[initSize];
+	nextIndex=0;	
+	}
+	
+	public void remove(int index){
+		if(isEmpty()){
+		if(index>=0;&&index < nextIndex){
+		for(int i =index;i<nextIndex-1;i++){
+			studentArray[i] = studentArray[i+1];
+		}	
+		}	
+		nextIndex--;	
+		}
+	}
+	
+	public void removeFirst(){
+		remove(0);
+	}
+	public void removeLast(){
+		remove(size()-1);
+	}
+	
+	
+	
+	
 	
 	
 	
 }
+
+
 
 
 class Demo{
